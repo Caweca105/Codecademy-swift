9e8e5a33ccdb7a442b18ec480232c6ec6dcf1ad1
Caweca105 <39473363+Caweca105@users.noreply.github.com>
2021-02-18 22:09:05 +0000

    new stuff

---
+++ PiggyBank.swift
@@ -0,0 +1,9 @@
+var pesos: Double = 4 //1 peso = 0.049$
+var reais: Double = 5 //1 real = 0.18$
+var soles: Double = 6 //1 sol = 0.27$
+
+var total: Double = 0
+
+total = 0.049 * pesos + 0.18 * reais + 0.27 * soles
+
+print("US Dollars = $\(total)")
---
+++ dogYears.swift
@@ -0,0 +1,7 @@
+var dogAge = 5 // Dog age is two
+
+var earlyYears = 21 // The first two years
+var laterYears = (dogAge - 2) * 4 // The following years
+var humanYears = earlyYears + laterYears // Your dog's age in human years
+
+print("My name is Rufffles! Ruff, ruff, I am \(humanYears) years old in human years.")
