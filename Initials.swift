3c6ef6cf4023e527902bd8181eed8e62603edf70
Caweca105 <39473363+Caweca105@users.noreply.github.com>
2021-02-18 15:23:43 +0000

    Create Initials.swift

---
+++ Initials.swift
@@ -0,0 +1,7 @@
+print(" DDDD   L") //I enjoy playing videogames
+print(" D   D  L") //bald people are awesome
+print(" D   D  L") //Yup, you read it correctly
+print(" D   D  L") //Baldies are awesome!
+print(" D   D  L")
+print(" D   D  L")
+print(" DDDD   LLLLL")
